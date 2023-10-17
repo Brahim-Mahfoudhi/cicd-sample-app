e35931b68601c7b7f7d1767a611bd32586f3f9c9
Brahim-Mahfoudhi <114872362+Brahim-Mahfoudhi@users.noreply.github.com>
2023-10-17 10:44:14 +0200

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-if (templates/tempdir) [
-  cd /templates
-  rm -r tempdir
-]
-else [
+if [[ -d templates/tempdir ]]; then
+cd /templates
+rm -r tempdir
+else 
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
-]
+
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
